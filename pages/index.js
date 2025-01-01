33a6596bf410dbfaf9a5aaa2914ed3a9cc1b1128
DonovanThuso93 <donovanmekgwe@gmail.com>
2025-01-01 23:32:24 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div className="container">
-      <main>
-        <h1>Welcome to Easy Store</h1>
-        <p>Your one-stop hardware price comparison platform</p>
-      </main>
+    <div>
+      <h1>Welcome to Easy Store</h1>
     </div>
-  );
+  )
 }
